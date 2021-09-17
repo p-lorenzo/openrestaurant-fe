43b48bef01a806faa9a5c84e5a328836e19a7bc1
p-lorenzo <lorenzo.pesce.1996@gmail.com>
2021-09-17 11:51:29 +0200

    Use axios interceptor for auth token

--- src/components/MenuEntries.js
+++ src/components/MenuEntries.js
@@ -2,11 +2,9 @@ import React, {useState} from "react";
 import axios from "axios";
 import {withRouter} from "react-router";
 import {DataGrid} from "@mui/x-data-grid";
-import authService from "../services/auth.service";
 
 const MenuEntries = () => {
     const [menuEntries, setMenuEntries] = useState(null);
-    axios.defaults.headers.common['X-AUTH-TOKEN'] = authService.getToken();
 
     React.useEffect(() => {
         axios.get(process.env.REACT_APP_API_URL + "/api/admin/menu-entry").then((response) => {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import authService from "./services/auth.service";
 
+axios.interceptors.request.use(
+    request => {
+        console.log(request.url.includes('admin'));
+        if (request.url.includes('admin')) {
+            request.headers['X-AUTH-TOKEN'] = authService.getToken();
+        }
+        return request;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
 
 ReactDOM.render(
     <React.StrictMode>
